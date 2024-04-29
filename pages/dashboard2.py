eb09f9470b1f46e2536da785db19fa3b3a1a6c1f
gregoryreis <greis003@fiu.edu>
2024-04-29 16:03:19 -0400

    Enhance interactivity by increasing data limit and map zoom level
    Changes have been made to the 'dashboard2.py' page, including the increase of the data limit fetched from 100 to 1000 records for better real-time visualization. The initial zoom level of the folium.Map feature has also been heightened from 20 to 18, providing a closer and potentially a more interactive view for the users.

--- pages/dashboard2.py
+++ pages/dashboard2.py
@@ -7,6 +7,8 @@ import folium
 from streamlit_folium import folium_static
 import time
 from streamlit_option_menu import option_menu
+import pydeck as pdk
+import plotly.express as px
 
 
 st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
@@ -14,7 +16,7 @@ st.sidebar.page_link("streamlit_app.py", label="Home", icon="🏠")
 st.sidebar.page_link("pages/realTime.py", label="Real-Time Data", icon="🚢")
 st.sidebar.page_link("pages/historicalData.py", label="Historical Data", icon="📊")
 st.sidebar.page_link("pages/ai3.py", label="BayBot (AI Tool)", icon="🤖")
-
+image_ = "Images/swarmIcon.png"
 menu = option_menu(
     menu_title=None,
     options=["Map - Real Time Visualization", "Charts - Real Time Visualization"],
@@ -41,7 +43,7 @@ color_dict = {'Chl (ug/L)': 'green',
 
 
 def fetch_latest_data_iot(x):
-    records = x.find().sort("timestamp", -1).limit(100)
+    records = x.find().sort("timestamp", -1).limit(1000)
     return pd.DataFrame(list(records))
 
 
@@ -60,8 +62,44 @@ if 'collection' in st.session_state:
 
     # tab1, tab2 = st.tabs(["Real Time Visualization on Map", "Real Time Visualization with Charts"])
     # with tab1:
-    if menu == "Map - Real Time Visualization":
 
+    if menu == "Map - Real Time Visualization":
+        def create_map_alt2(dataframe):
+            fig_map = px.scatter_mapbox(dataframe, lat='latitude', lon='longitude', hover_data=df, zoom=17,size_max=100)
+            fig_map.update_layout(mapbox_style="open-street-map")
+            st.plotly_chart(fig_map,use_container_width=True)
+        def create_map_alt(partial_ds):
+            center_lat, center_lon = partial_ds['latitude'].mean(), partial_ds['longitude'].mean()
+            st.pydeck_chart(pdk.Deck(
+                map_style= None,
+                # map_style='mapbox://styles/mapbox/satellite-streets-v11',
+                initial_view_state=pdk.ViewState(
+                    latitude=center_lat,
+                    longitude=center_lon,
+                    zoom=25,
+                    pitch=50,
+                ),
+                layers=[
+                    pdk.Layer(
+                        'ScatterplotLayer',
+                        # data=partial_ds[['lat','lon']],
+                        data=partial_ds,
+                        get_position='[longitude, latitude]',
+                        get_color='[26, 255, 0, 160]',
+                        get_radius=0.25,
+                        pickable=True,  # to show tooltip
+                    ),
+                ],
+                tooltip={
+                    # "html": "Lat: {lat} <br/> Long:{lon}",
+                    # create 0s for the options when not selected
+                    "html": "Lat: {latitude} <br/> Long:{longitude} <br/> Time: {Time}",
+                    "style": {
+                        "backgroundColor": "blue",
+                        "color": "white"
+                    }
+                }
+            ))
         def create_map(data):
             # Create a map centered around an average coordinate
             if not data.empty:
@@ -71,35 +109,54 @@ if 'collection' in st.session_state:
                 center_lat, center_lon = 0, 0
 
             # Start map at the computed average location
-            my_map = folium.Map(location=[center_lat, center_lon], zoom_start=20)
+            my_map = folium.Map(location=[center_lat, center_lon],
+                                zoom_start=18,
+                                control_scale=True,
+                                prefer_canvas=True)
 
             # Add points from the data
             for index, item in data.iterrows():
                 tooltip = (
-                    f"Date: {item.get('Date', 'N/A')}, Time: {item.get('Time', 'N/A')}, \n"
-                    f"Chl (ug/L): {item.get('Chl (ug/L)', 'N/A')}, BGA-PE (ug/L): {item.get('BGA-PE (ug/L)', 'N/A')}, \n"
-                    f"Turb (FNU): {item.get('Turb (FNU)', 'N/A')}, TSS (mg/L): {item.get('TSS (mg/L)', 'N/A')}, \n"
-                    f"ODO (%sat): {item.get('ODO (%sat)', 'N/A')}, ODO (mg/l): {item.get('ODO (mg/l)', 'N/A')}, \n"
-                    f"Temp (C): {item.get('Temp (C)', 'N/A')}, Cond (uS/cm): {item.get('Cond (uS/cm)', 'N/A')}, \n"
-                    f"Sal (PPT): {item.get('Sal (PPT)', 'N/A')}, Pressure (psi a): {item.get('Pressure (psi a)', 'N/A')}, \n"
-                    f"Depth (m): {item.get('Depth (m)', 'N/A')}, latitude: {item.get('latitude', 'N/A')}, "
+                    f"Date: {item.get('Date', 'N/A')} <br/> Time: {item.get('Time', 'N/A')} <br/>"
+                    # f"Chl (ug/L): {item.get('Chl (ug/L)', 'N/A')}, BGA-PE (ug/L): {item.get('BGA-PE (ug/L)', 'N/A')}, \n"
+                    # f"Turb (FNU): {item.get('Turb (FNU)', 'N/A')}, TSS (mg/L): {item.get('TSS (mg/L)', 'N/A')}, \n"
+                    # f"ODO (%sat): {item.get('ODO (%sat)', 'N/A')}, ODO (mg/l): {item.get('ODO (mg/l)', 'N/A')}, \n"
+                    # f"Temp (C): {item.get('Temp (C)', 'N/A')}, Cond (uS/cm): {item.get('Cond (uS/cm)', 'N/A')}, \n"
+                    # f"Sal (PPT): {item.get('Sal (PPT)', 'N/A')}, Pressure (psi a): {item.get('Pressure (psi a)', 'N/A')}, \n"
+                    f"Depth (m): {item.get('Depth (m)', 'N/A')} <br/> latitude: {item.get('latitude', 'N/A')} <br/>"
                     f"longitude: {item.get('longitude', 'N/A')}"
                 )
-                folium.Marker(
+                normal_marker=folium.Marker(
                     [item['latitude'], item['longitude']],
-                    tooltip=tooltip
-                ).add_to(my_map)
-
+                    tooltip=tooltip,
+                )
+                circle_marker=folium.Circle(location=[item['latitude'], item['longitude']],
+                              radius=0.3,
+                              color='orange',
+                              tooltip=tooltip,
+                              fill=True)
+                icon = folium.CustomIcon(
+                    image_,
+                    icon_size=(15, 30),
+                    # icon_anchor=(22, 94),
+                )
+                swarm_marker = folium.Marker(
+                    [item['latitude'], item['longitude']],
+                    tooltip=tooltip,
+                    icon=icon
+                )
+                swarm_marker.add_to(my_map)
             return my_map
-        data= fetch_latest_data_iot(st.session_state['collection'])
-        my_map = create_map(data)  # Create the map
-        folium_static(my_map, width=1550, height=800)  # Display the map in Streamlit
+        # data= fetch_latest_data_iot(st.session_state['collection'])
+        my_map = create_map(df)  # Create the map
+        folium_static(my_map, width=1550, height=800)
+
         # time.sleep(5)
     if menu == "Charts - Real Time Visualization":
     # with tab2:
         # Variables to plot (excluding Date, Time, and DateTime)
-        variables_to_plot = ['Chl (ug/L)', 'BGA-PE (ug/L)', 'Turb (FNU)', 'TSS (mg/L)',
-                             'ODO (%sat)', 'ODO (mg/l)', 'Temp (C)', 'Cond (uS/cm)',
+        variables_to_plot = ['Temp (C)', 'Chl (ug/L)', 'BGA-PE (ug/L)', 'Turb (FNU)', 'TSS (mg/L)',
+                             'ODO (%sat)', 'ODO (mg/l)', 'Cond (uS/cm)',
                              'Sal (PPT)', 'Pressure (psi a)', 'Depth (m)']
 
         # Streamlit selectbox widget
@@ -132,6 +189,7 @@ if 'collection' in st.session_state:
     if st.button('Reset Session'):
         st.session_state.clear()
         st.switch_page("streamlit_app.py")
+    st.divider()
 else:
     st.error("No collection selected or session expired.")
     st.button("Go Back", on_click=st.switch_page, args=("streamlit_app.py",))
